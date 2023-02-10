09d88f16ee85cc9bf0240f87dea2d10eca54ed05
VladimirShch <43771483+VladimirShch@users.noreply.github.com>
2023-02-10 21:35:21 +0300

    New animations all moves in one image and one image per direction

--- game.js
+++ game.js
@@ -25,10 +25,12 @@ class Character
 		this.endingFrame = 4 * this.framesPerImage; // Потому что 4 изображения всего на анимацию
 		this.frame = this.startingFrame;
 		this.image = new Image();
-		this.image.src = `${this.characterName}/stand.png`;
-		this.angle = "90";
-		this.image.onload = () => this.image.style.transform = `rotate(${this.angle}deg)`;
-		this.rotation = 0; // радианы
+		this.image.src = `${this.characterName}/Move/down.png`;
+		this.previousDirection = "d";
+		//this.animationNum = 0;
+		//this.angle = "90";
+		//this.image.onload = () => this.image.style.transform = `rotate(${this.angle}deg)`;
+		//this.rotation = 0; // радианы
 	}
 	
 	Move(direction)
@@ -37,43 +39,48 @@ class Character
 		{ 
 			//this.image.style.transform = "rotate(0deg)";
 			//this.rotation = 0;
+			this.image.src = `${this.characterName}/Move/right.png`;
 			this.x++;
 		}
 		else if(direction == "l")
 		{
-			this.angle = '180';
+			//this.angle = '180';
 			//this.image.style.transform = "rotate(180deg)";
 			//this.rotation = 3,14159;
+			this.image.src = `${this.characterName}/Move/left.png`;
 			this.x--;
 		}
 		else if(direction == "d")
 		{
 			//this.image.style.transform = "rotate(270deg)";
 			//this.rotation = -1,5708;
+			this.image.src = `${this.characterName}/Move/down.png`;
 			this.y++;
 		}
 		else if(direction == "u")
 		{
 			//this.image.style.transform = "rotate(90deg)";
 			//this.rotation = 1,5708;
+			this.image.src = `${this.characterName}/Move/up.png`;
 			this.y--;
-		}		
+		}
+		
+		if(direction !== this.previousDirection){
+			this.frame = 0;
+		}
+
+		this.previousDirection = direction;
 	}
 	
 	SetAnimation()
 	{
 		if(this.x == this.lastX && this.y == this.lastY)
 		{
-			//this.frame++;
-			//if(this.frames >= this.framesPerImage)
-			//{
-				this.frame = this.startingFrame;
-				this.image.src = `${this.characterName}/stand.png`;
-			//}			
+			this.frame  = 0;
 		}
 		else
 		{
-			this.image.src  = `${this.characterName}/Move/${(this.frame/this.framesPerImage + 1)}.png`;
+			//this.image.src  = `${this.characterName}/Move/${(this.frame/this.framesPerImage + 1)}.png`;
 			if(this.frame == this.endingFrame - 1)
 			{
 				this.frame = this.startingFrame;				
@@ -156,13 +163,13 @@ function Update(city, character)
 	context.drawImage
 	(
 		character.image,
+		Math.floor(character.frame/character.framesPerImage) * (character.image.width / 4),
 		0,
-		0,
-		character.image.width,
+		character.image.width / 4,
 		character.image.height,
 		character.x,
 		character.y,
-		character.image.width,
+		character.image.width / 4,
 		character.image.height
 	);
 	//context.rotate(-character.rotation);
@@ -171,21 +178,21 @@ function Update(city, character)
 }
 
 function characterSelection(){
-	const selectText = new Image();
-	selectText.src = "Select_character.png";
-	selectText.onload = () => context.drawImage(selectText, canvas.width / 2 - 90, 10);
+	const selectTextImage = new Image();
+	selectTextImage.src = "Select_character.png";
+	selectTextImage.onload = () => context.drawImage(selectTextImage, canvas.width / 2 - selectTextImage.width / 2, 0);
 
 	const markImage = new Image();
 	markImage.src = "Mark/mark_charlist.png";
-	markImage.onload = () => context.drawImage(markImage, canvas.width / 2 - markImage.width / 2, 50);
+	markImage.onload = () => context.drawImage(markImage, canvas.width / 2 - markImage.width / 2, 100);
 	
 	const amandaImage = new Image();
 	amandaImage.src = "Amanda/amanda_charlist.png";
-	amandaImage.onload = () => context.drawImage(amandaImage, canvas.width/2 - amandaImage.width / 2, 50 + markImage.height + 10);
+	amandaImage.onload = () => context.drawImage(amandaImage, canvas.width/2 - amandaImage.width / 2, 100 + markImage.height + 10);
 	
 	const michaelImage = new Image();
 	michaelImage.src = "Michael/michael_charlist.png";
-	michaelImage.onload = () => context.drawImage(michaelImage, canvas.width/2 - michaelImage.width / 2, 50 + markImage.height + amandaImage.height + 20);
+	michaelImage.onload = () => context.drawImage(michaelImage, canvas.width/2 - michaelImage.width / 2, 100 + markImage.height + amandaImage.height + 20);
 
 	canvas.onclick = (e) => {
 		if(e.x >=  canvas.width / 2 - markImage.width / 2 && e.x <=  canvas.width / 2 + markImage.width / 2 && 
